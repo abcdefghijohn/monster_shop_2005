5bbb7156d38c21110e454a0c15da74f8d119d29f
John Kim <abcdefghijohnk@gmail.com>
2020-11-07 21:25:23 -0700

    Create feature test for discounts index

---
+++ spec/features/merchant_user/discounts/show_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Dashboard Show' do
+  describe 'When I visit the merchant dashboard show page' do
+    before(:each) do
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+      @tire = @meg.items.create(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
+      @chain = @meg.items.create(name: 'Chain', description: "It'll never break!", price: 50, image: 'https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588', inventory: 5)
+      @shifter = @meg.items.create(name: 'Shimano Shifters', description: "It'll always shift!", active?: false, price: 180, image: 'https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg', inventory: 2)
+      @discount_1 = @meg.discounts.create(discount_percent: 5, min_quantity: 5)
+      @discount_2 = @meg.discounts.create(discount_percent: 10, min_quantity: 10)
+
+      user = @meg.users.create(name: 'JakeBob',
+                               address: '124 Main St',
+                               city: 'Denver',
+                               state: 'Colorado',
+                               zip: '80202',
+                               email: 'JBob1234@hotmail.com',
+                               password: 'heftybags',
+                               password_confirmation: 'heftybags',
+                               role: 1)
+
+      visit '/login'
+
+      fill_in :email, with: 'JBob1234@hotmail.com'
+      fill_in :password, with: 'heftybags'
+      click_button 'Login'
+    end
+
+    it 'I see a link to all of my discounts' do
+      expect(page).to have_link("My Discounts")
+      click_on("My Discounts")
+
+      expect(page).to have_content(@discount_1.discount_percent)
+      expect(page).to have_content(@discount_1.min_quantity)
+
+      expect(page).to have_content(@discount_2.discount_percent)
+      expect(page).to have_content(@discount_2.min_quantity)
+    end
+  end
+end
